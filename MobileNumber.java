b7b37d46c5dd830b49fd33cdd0168fa342c5f714
GaneshPH <113449116+GaneshPH@users.noreply.github.com>
2022-09-14 22:28:49 +0530

    Add files via upload

---
+++ MobileNumber.java
@@ -0,0 +1,59 @@
+class MobileNumber{
+	 static String getNameByMobile(long number){
+		 
+		 System.out.println("entered getnameByMobileNumber");
+		 if(number==9449565944L)
+		 {
+			 System.out.println("ganesh mobile number");
+			 return"ganesh";	 
+		 }
+		 if(number==9449565945L)
+		 {
+			 System.out.println("sanjay Mobile Number");
+			 return"sanjay";
+		 }
+		 if(number==9449565946L)
+		 {
+			 System.out.println(" prashanth mobile number");
+			 return"prashanth";
+		 }
+		 if(number==9449565947L)
+		 {
+			 System.out.println("sharath mobile number");
+			 return"sharath";
+		 } 
+		 if(number==9449565948L)
+		 {
+			 System.out.println("arun mobile number");
+			 return"arun";
+		 }
+		 if(number==9449565949L)
+		 {
+			 System.out.println("shashank mobile number");
+			 return"shashank";
+		 }
+		 if(number==9449565950L)
+		 {
+			 System.out.println("nataraj mobile number");
+			 return"nataraj";
+		 }
+		if(number==9449565951L)
+		{
+			System.out.println("howduhuliya mobile number");
+			return"howduhuliya";
+		}
+        if(number==9449565952L)
+	    {
+			System.out.println("howduhuliya2 mobile number");
+			return"howduhuliya2";
+		}	
+	   if(number==9449565953L)
+	   {
+	     System.out.println("naveen mobile number");
+		 return"naveen";
+	   }
+	return null;
+	
+	
+	 }
+}
---
+++ MobileNumberRunner.java
@@ -0,0 +1,9 @@
+class MobileNumberRunner
+{
+	public static void main(String[]name)
+	{
+		long mobile=9449565944L;
+		String ref=MobileNumber.getNameByMobile(mobile);
+		System.out.println(ref);
+	}
+}
