a846259220371311c0f3091aaddc801735f934e0
GaneshPH <ganeshph.xworkz@gmail.com>
2022-09-19 22:30:38 +0530

    arrays

---
+++ SpiceJetRunner.java
@@ -0,0 +1,8 @@
+class SpiceJetRunner{
+	public static void main(String[] Jets)
+	{
+		System.out.println("flight number");
+		String[] FlightNo={"SG31","SG131","SG8264","SG4050","SG467","SG8709","SG116","SG922","SG2971","SG740","SG8158"};
+		SpiceJet.flight(FlightNo);
+	}
+}
