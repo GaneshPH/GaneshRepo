e133fb9f04decb3de3acab79df9c06added47061
GaneshPH <ganeshph.xworkz@gmail.com>
2022-09-21 10:46:26 +0530

    updated

--- Card.java
+++ Card.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Card{
 	static String type;
 	static String height;
@@ -26,35 +25,5 @@ class Card{
 		
 	}
 
-
-=======
-class Card{
-	static String type;
-	static String height;
-	static int width;
-	static double price;
-	static String[] color;
-	
-	
-	static void displayDetails(){
-		
-	System.out.println(type);
-	System.out.println(height);
-	System.out.println(width);
-	System.out.println(price);
-	if(color!=null){
-		for(int position=0;position<color.length;position++)
-			{
-			String ref=color[position];
-			System.out.println(ref+" "+position);
-			}
-	}
-	else{
-	System.out.println("Array is null");
-	}
-		
-	}
-
-
->>>>>>> f7a18ce8db584a40633386e272fc6afcf9abc231
+}
 }
